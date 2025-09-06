8339e1c2cecee0d67f1510964ebe564627e02d6e
sirakaba <sirakaba@SIRAKABA-DESKTOP.localdomain>
2025-09-06 14:06:45 +0900

    認証機能の確認(TOTPのQRコード画面の表示まで学習)

--- Readme.md
+++ Readme.md
@@ -1,9 +1,13 @@
+# 目標
+dockerを使用して二段階認証のアプリケーションサイトを作成する
+http://localhost:8080/
+
 # dockerの起動方法
 
 ## 1. コンテナの起動
 
 ```shell
-$ docker-compose up -d
+$ docker-compose up -d --build
 ```
 
 ## 2. コンテナの停止
@@ -15,7 +19,11 @@ $ docker-compose down
 
 #　履歴
 
-2025/03/01 PHPサイト作成（ログイン周り）
+2025/03/01 PHP導入
+2025/09/06 認証機能の確認(TOTPのQRコード画面の表示まで学習)
+
+次回　ログイン画面の作成
+
 
 ## 参考資料
 https://blog.apar.jp/program/12741/
--- html/app/index.php
+++ html/app/index.php
@@ -1,3 +1,27 @@
 <?php
  
-phpinfo();
+require_once 'source/PHPGangsta/GoogleAuthenticator.php';
+
+$ga = new PHPGangsta_GoogleAuthenticator();
+$secret = $ga->createSecret();
+$title = 'MyTestApp';
+$name = 'sirakaba';
+
+$qrCodeUrl = $ga->getQRCodeGoogleUrl($name, $secret, $title);
+echo "秘密鍵のQRコードURL:{$qrCodeUrl}\n\n";
+
+// $secret = 'EQXAPJ6ISAK5VM7U';
+// // ユーザーが生成したTOTP
+// $oneCode = filter_input($ga->getCode($secret), 'oneCode');
+// // サーバーとクライアントで許容する時間のずれ
+// $discrepancy = 2;
+
+// $checkResult = $ga->verifyCode($secret, $oneCode, $discrepancy);    // 2 = 2*30sec clock tolerance
+
+// if ($checkResult) {
+//     echo 'OK';
+// } else {
+//     echo 'FAILED';
+// }
+
+
